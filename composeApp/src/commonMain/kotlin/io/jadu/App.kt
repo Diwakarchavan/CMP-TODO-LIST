ff2c0466736e5d46812b3f9d33654073464866f0
Divakar <divakarchavan@gmail.com>
2026-04-11 11:13:38 +0530

    Rename `DARK_MODE_KEY` to `darkModeKey` in `ThemeRepository`
    - Update `DARK_MODE_KEY` to follow camelCase naming convention for the private property.
    - Refactor all references to use the renamed `darkModeKey`.

--- composeApp/src/commonMain/kotlin/io/jadu/App.kt
+++ composeApp/src/commonMain/kotlin/io/jadu/App.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import io.jadu.todoApp.data.local.ThemeRepository
 import io.jadu.todoApp.ui.screens.MainScreen
@@ -13,7 +12,6 @@ import io.jadu.todoApp.ui.theme.TodoAppTheme
 import org.koin.compose.koinInject
 
 @Composable
-@Preview
 fun App() {
     val themeRepository: ThemeRepository = koinInject()
     val isDarkMode by themeRepository.isDarkMode.collectAsState(initial = null)
